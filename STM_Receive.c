ae010c7f293f1ac30036fefbc46d1b9ffd733ceb
YukiShigematsu <71487860+YukiShigematsu@users.noreply.github.com>
2021-12-15 23:35:17 +0900

    Update STM_Receive.c

--- STM_Receive.c
+++ STM_Receive.c
@@ -1,30 +1,14 @@
 /*
 /	GPL-Ver3.0 License
 /
-/	Copyright (c) 2021, Yuki Shigematsu.
+/	Copyright (c) 2021, Yuki Shigematsu & STMicroelectronics.
 /	All rights reserved.
 /	The parts of this program except lines 67, 107-129, and 275-441 are originally BSD licenses.
 /
 */
 
 /* USER CODE BEGIN Header */
-/**
-  ******************************************************************************
-  * @file           : main.c
-  * @brief          : Main program body
-  ******************************************************************************
-  * @attention
-  *
-  * <h2><center>&copy; Copyright (c) 2021 STMicroelectronics.
-  * All rights reserved.</center></h2>
-  *
-  * This software component is licensed by ST under BSD 3-Clause license,
-  * the "License"; You may not use this file except in compliance with the
-  * License. You may obtain a copy of the License at:
-  *                        opensource.org/licenses/BSD-3-Clause
-  *
-  ******************************************************************************
-  */
+
   /* USER CODE END Header */
   /* Includes ------------------------------------------------------------------*/
 #include "main.h"
